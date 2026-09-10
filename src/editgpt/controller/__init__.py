5eb84d7e7484965f754ea025ddc2814750710fcd
Dashesthe1 <olufemioluwaseun777@gmail.com>
2026-09-10 14:03:55 -0500

    Export M4 editing task contract

--- src/editgpt/controller/__init__.py
+++ src/editgpt/controller/__init__.py
@@ -1,12 +1,16 @@
 from .coordinates import CoordinateTransform
+from .edit_task import EditingTaskContract, ScopeDecision, verify_rollback
 from .planner import PlannedAction, choose_next_action, verify_visible_state
 from .semantic_pointer import SemanticPointerTarget, choose_pointer_target
 
 __all__ = [
     "CoordinateTransform",
+    "EditingTaskContract",
     "PlannedAction",
+    "ScopeDecision",
     "SemanticPointerTarget",
     "choose_next_action",
     "choose_pointer_target",
+    "verify_rollback",
     "verify_visible_state",
 ]
